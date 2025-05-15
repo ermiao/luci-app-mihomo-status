d5eaf8cef3e32ec57ff38c7edfa22d3d671582d4
学徒鹌鹑 <ermiao@gmail.com>
2025-05-15 16:39:52 +0800

    Delete luci-app-mihomo-status directory

--- luci-app-mihomo-status/luasrc/controller/mihomo_status.lua
+++
@@ -1,6 +0,0 @@
-
-module("luci.controller.mihomo_status", package.seeall)
-
-function index()
-    entry({"admin", "status", "mihomo_status"}, template("mihomo_status/status"), _("Mihomo 状态"), 90)
-end
